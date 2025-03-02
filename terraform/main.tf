74799d2d770b8be5c0a180c02a16f35683afc604
Shihab Saheer <shihabshaheer8@gmail.com>
2025-03-02 10:06:08 +0530

    READme file updated and output.tf created

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,15 +3,7 @@ resource "local_file" "test1" {
   content = "test file"
 }
 
-output "filename" {
-  value = local_file.test1.filename
-}
-
 resource "local_file" "test2" {
   filename = var.file2
   content = "test file2"
-}
-
-output "filename2" {
-  value = local_file.test2.filename
 }
---
+++ terraform/output.tf
@@ -0,0 +1,7 @@
+output "filename" {
+  value = local_file.test1.filename
+}
+
+output "filename2" {
+  value = local_file.test2.filename
+}
